15adb7f8d7d7977b5c9451760938fc7c23f45843
IshaRode <you@example.com>
2024-01-18 15:59:37 +0530

    created basic html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import "./App.css";
-import questions from "./questions";
+// import questions from "./questions";
 import Result from "./components/Result";
 import QuestionBox from "./components/QuestionBox";
 
@@ -10,7 +10,8 @@ function App() {
 
   return (
     <div>
-      
+    <QuestionBox/>
+    <Result/>
     </div>
   );
 }
--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import './QuestionBox.css'
 
 export default function QuestionBox() {
   return (
-    <div>
+    <div className='container'>
+      <div className='quiz'>
+        <div className='fifteen'>
+        <p >1 of 5</p>
+        </div>
+        <p className='mammal'>What is react-js</p>
+        <div className='options'>
+        
+            <button className='opt'>Option-1</button>
+            <button className='opt'>Option-2</button>
+            <button className='opt'>Option-3</button>
+            <button className='opt'>Option-4</button>
+        
+        </div>
+        <div className='highlight-buttons'>
+          <button className='previous' >Highlight</button>
+          <button className='next' >Unhighlight</button>
+        </div>
+
+      </div>
       
     </div>
   )
